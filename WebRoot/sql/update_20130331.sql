8ba9a96f3ff7561bd2f940e655b93b3775bb900d
zhangjie1983 <417430344@qq.com>
2013-04-01 02:10:36 +0800

    版本提交2013/4/1

---
+++ WebRoot/sql/update_20130331.sql
@@ -0,0 +1,9 @@
+/****** 说明
+	如系统于2013年3月31日就已搭建，那么执行这个sql
+ ******/
+-----------------------------------------------------
+-- Export file for user NJMD                     --
+-- Created by Administrator on 2013/3/10, 15:46:34 --
+-----------------------------------------------------
+ALTER TABLE frame_user ADD ROLE_TYPE NUMBER(1);
+commit;
